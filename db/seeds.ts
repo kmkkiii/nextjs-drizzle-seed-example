2e6ad1391ece233be9f78f590a58bac793d1c40b
kmkkiii <rskkmkmk@gmail.com>
2024-12-01 12:09:59 +0900

    add weightedRandom

--- db/seeds.ts
+++ db/seeds.ts
@@ -5,17 +5,31 @@ import { db } from './client';
 async function main() {
   await seed(db, schema).refine((f) => ({
     usersTable: {
-      count: 5,
+      count: 10,
       columns: {
         id: f.int({
           minValue: 1,
           isUnique: true,
         }),
         name: f.fullName(),
-        age: f.int({
-          minValue: 20,
-          maxValue: 100,
-        }),
+        age: f.weightedRandom([
+          {
+            weight: 0.5,
+            value: f.int({ minValue: 20, maxValue: 39 }),
+          },
+          {
+            weight: 0.2,
+            value: f.int({ minValue: 40, maxValue: 49 }),
+          },
+          {
+            weight: 0.2,
+            value: f.int({ minValue: 50, maxValue: 59 }),
+          },
+          {
+            weight: 0.1,
+            value: f.int({ minValue: 60, maxValue: 100 }),
+          },
+        ]),
       },
     },
     postsTable: {
